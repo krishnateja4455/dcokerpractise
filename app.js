94b2e785d02951337db64c827483d074b0a6811e
krishna <krishnaalphak9@gmail.com>
2025-06-06 12:49:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+
+app.use(cors());
+
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"home page"});
+});
+
+app.post('/contact',(req,res)=>{
+    res.status(200).json({message:"contact page"});
+})
+
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+
+
+const port = 5000;
+
+
+app.listen(port,()=>{
+  console.log(`server running on ${port}`);
+});
